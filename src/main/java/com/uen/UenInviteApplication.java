53f0d13141ba5691d1b46c20c091242494375533
znb <znb@qq.com>
2019-07-09 23:47:56 +0800

    Initial commit

---
+++ src/main/java/com/uen/UenInviteApplication.java
@@ -0,0 +1,13 @@
+package com.uen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UenInviteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UenInviteApplication.class, args);
+    }
+
+}
